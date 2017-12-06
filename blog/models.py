e9599f5d0fe2dbc606331338cf00fe98bb1631ec
zhengjiali <zhengjiali@zhengjldeMacBook-Air.local>
2017-12-06 18:37:35 +0800

    add css

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published')
+
 class Post(models.Model):
 	STATUS_CHOICES = (
 		('draft','Draft'),
@@ -16,12 +21,25 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+	objects = models.Manager() #The default manager.
+	published = PublishedManager()# Our custom manager.
+
 
 	class Meta:
 		ordering = ('-publish',)
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('blog:post_detail',
+			args=[self.publish.year,
+			self.publish.strftime('%m'),
+			self.publish.strftime('%d'),
+			self.slug
+			])
+
+
 
 # class log(models.Model):
 # 	positionId = models.Index()
