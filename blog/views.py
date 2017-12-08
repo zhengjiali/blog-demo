b8f07c3a4777c971bc755fedfcd32a32b60e4481
zhengjiali <zhengjiali@zhengjldeMacBook-Air.local>
2017-12-08 18:39:04 +0800

    use class-based views

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-
+from django.views.generic import ListView
 # Create your views here.
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
 
 def post_list(request):
 	object_list = Post.published.all()
