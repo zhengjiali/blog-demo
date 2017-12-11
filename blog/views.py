b4cfadebdd26c6addeb55ffc8150889192d7fcf3
zhengjiali <zhengjiali2014@163.com>
2017-12-11 16:25:18 +0800

    share post

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,35 @@ from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
 # Create your views here.
+
+def post_share(request,post_id):
+	#Retrieve post by id
+	# post = get_object_or_404(Post,id=post_id)
+	sent = False
+	try:
+		post = Post.objects.get(id=post_id)
+	except:
+		return Http404
+
+	if request.method == 'POST':
+		#Form was submitted
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			# Form fields passed validation
+			cd = form.cleaned_data
+			post_url = request.build_absolute_uri(
+				post.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"' .format(cd['name'],cd['email'],post.title)
+			message = 'Read "{}" at {} \n\n {}\'s comments:{}' .format(post.title,post_url,cd['name'],cd['comments'])
+			send_mail(subject, message, 'zhengjiali2014@163.com', [cd['to']])
+			sent = True
+	else:
+		form = EmailPostForm()
+	return render(request, 'blog/post/share.html',{'post':post,'form':form,'sent':sent})
 
 class PostListView(ListView):
 	queryset = Post.published.all()
