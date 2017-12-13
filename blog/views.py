3b72bc6bd7aecb9646c88f50695666eab306deac
zhengjiali <zhengjiali2014@163.com>
2017-12-13 18:45:55 +0800

    1

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,Comment
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 
 # Create your views here.
@@ -56,6 +56,20 @@ def post_list(request):
 def post_detail(request,year,month,day,post):
 	try:
 		post = Post.objects.get(slug=post)
+		comments = post.comments.filter(active=True)
+
+		if request.method == 'POST':
+			# A comment was posted
+			comment_form = CommentForm(data=request.POST)
+			if comment_form.is_valid():
+				# Create Comment object but don't save to database yet
+				new_comment = comment_form.save(commit=False)
+				# Assign the current post to the comment
+				new_comment.post=post
+				# Save the comment to the database
+				new_comment.save()
+		else:
+			comment_form = CommentForm()
 	except:
 		return Http404
 	# post = get_object_or_404(Post,slug=post,
@@ -64,4 +78,4 @@ def post_detail(request,year,month,day,post):
 	# 	publish__month=month,
 	# 	publish__day=day)
 	return render(request,'blog/post/detail.html', 
-		{'post':post})
+		{'post':post,'comments':comments,'comment_form':comment_form})
